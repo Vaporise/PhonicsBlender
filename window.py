0f3d67a3b50177fe3f8ebadd87c9f7e184a264b6
Vaporise <draxdragonfire@googlemail.com>
2025-02-06 12:16:40 +0900

    Added some comments to explain the code better

--- window.py
+++ window.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 
 class DragDropApp:
-    def __init__(self, window):
+    def __init__(self, window): 
         self.window = window
-        self.initialize_window()
+        self.initialize_window() #runs the initialize_window method at runtime
 
-    def initialize_window(self):
+    def initialize_window(self): #creates a window with a title, size and runs create_widgets
         self.window.title("Phonics Blender")
         self.window.geometry("900x900")
         self.create_widgets()
     
-    def create_widgets(self):
+    def create_widgets(self): #creates widgets for the window of a label and a frame with a label within
         label = tk.Label(self.window, text="Hello, Tkinter!", font=("Arial", 16))
         label.pack(pady=20)
 
@@ -22,9 +22,9 @@ class DragDropApp:
 
 
 def main():
-    window = tk.Tk()
-    app = DragDropApp(window)
-    window.mainloop()
+    window = tk.Tk() # Creates the window
+    app = DragDropApp(window) #for future use. It's an instance of the app.
+    window.mainloop() #runs the window and loops it to update with user interaction.
 
 if __name__ == "__main__":
     main()
